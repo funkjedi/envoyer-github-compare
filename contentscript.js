524ac894f7daf909ef5b3dda1b7f1d3efe4b7c0e
Tim Robertson <funkjedi@gmail.com>
2018-08-17 13:55:01 -0400

    Updated for new VueJS version of Envoyer

--- contentscript.js
+++ contentscript.js
@@ -5,11 +5,14 @@ compareBtn.className = 'btn btn-default';
 compareBtn.innerHTML = '<i class="fa fa-stack-overflow with-text"></i> Compare';
 
 compareBtn.onclick = function() {
-	var branch = document.querySelector('.branch-name');
-	var commit = document.querySelector('#recent-deployments .panel-body table tr:first-child td span[ng-if="project.provider == \'github\'"] a');
-	if (branch && commit) {
-		var link = commit.href.replace(/commit/,'compare') + '...' + branch.innerText;
-		window.open(link, '_blank');
+	var container = document.querySelector('#recent-deployments .card-body table tr:first-child td:nth-child(3) div');
+	if (container) {
+		var branch = container.innerText.replace(/^.*\((.*)\).*$/, '$1');
+		var commit = container.querySelector('a');
+		if (commit) {
+			var link = commit.href.replace(/commit/,'compare') + '...' + branch;
+			window.open(link, '_blank');
+		}
 	}
 };
 
@@ -17,7 +20,7 @@ compareBtn.onclick = function() {
 // check inside interval as angular may not have
 // initiated yet at the time the extention content script is run
 var intervalID = setInterval(function() {
-	var deployBtn = document.querySelector('button[ng-click="showDeployProjectModal()"]');
+	var deployBtn = document.querySelector('#app > div > .section-header button.btn-primary');
 	if (deployBtn) {
 		clearInterval(intervalID);
 		deployBtn.insertAdjacentElement('beforebegin', compareBtn);
